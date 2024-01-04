e2a350c17e9fbbac192c540297425e4561c2c3a1
gustavomoraispereira <gustavo.pereira@dotgroup.com.br>
2024-01-04 14:12:32 -0300

    - Configured rabbitmq container service - Next step is learn how to manage queues and start rabbitmq service

--- symfony_api/config/bundles.php
+++ symfony_api/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Liip\FunctionalTestBundle\LiipFunctionalTestBundle::class => ['dev' => true, 'test' => true],
+    OldSound\RabbitMqBundle\OldSoundRabbitMqBundle::class => ['all' => true],
 ];
