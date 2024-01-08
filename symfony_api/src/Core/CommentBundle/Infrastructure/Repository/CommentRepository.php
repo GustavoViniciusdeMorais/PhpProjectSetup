4432a51c0b00154ea2ab039ccb2df7c683f94b2c
gustavomoraispereira <gustavo.pereira@dotgroup.com.br>
2024-01-08 14:04:49 -0300

    - Added entity repository from doctrine to comment repository infrastructure

--- symfony_api/src/Core/CommentBundle/Infrastructure/Repository/CommentRepository.php
+++ symfony_api/src/Core/CommentBundle/Infrastructure/Repository/CommentRepository.php
@@ -4,11 +4,14 @@ namespace App\Core\CommentBundle\Infrastructure\Repository;
 
 use App\Core\CommentBundle\Domain\Entity\Comment;
 use App\Core\CommentBundle\Domain\Repository\CommentRepositoryInterface;
+use Doctrine\ORM\EntityRepository;
 
-class CommentRepository implements CommentRepositoryInterface
+class CommentRepository extends EntityRepository implements CommentRepositoryInterface
 {
     public function save(Comment $comment): Comment
     {
+        $this->_em->persist($comment);
+        $this->_em->flush();
         return new Comment();
     }
 }
