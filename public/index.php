2a27d3b50daf01e3a3f3230624ddef6c27d494c3
gustavomoraispereira <gustavo.pereira@dotgroup.com.br>
2024-01-12 18:24:06 -0300

    - Started simple soap example. The tutorial is done at md file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,20 @@
 <?php
 
-print_r(json_encode([
-    'Container' => 'PHP'
-]));echo "\n\n";
+try {
+    // Create a SOAP client
+    $client = new \SoapClient(null, array(
+        'location' => 'http://localhost/server.php',
+        'uri' => 'http://localhost/',
+    ));
+
+    // Call the sayHello function on the server
+    $response = $client->sayHello('John');
+
+    // Print the response
+    echo $response;
+    //code...
+} catch (\Exception $e) {
+    echo "Message: {$e->getMessage()}\n";
+    $details = json_encode($e->getTrace());
+    echo "Details: {$details}\n";
+}
